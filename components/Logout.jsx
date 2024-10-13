46db6503df42c37dd4f75ccbbe05631b3de43cfd
Obi.É <Obada.Erfan97@gmail.com>
2024-10-13 10:33:37 -0400

    Minor updates

--- components/Logout.jsx
+++ components/Logout.jsx
@@ -1,13 +1,10 @@
 "use client"
 import React from 'react'
 import Button from './Button'
-import {useAuth} from "@/context/AuthContext" 
-import { usePathname } from 'next/navigation'
-import { Link } from 'react-router-dom'
+import {useAuth} from "@/context/AuthContext"
 
 export default function Logout() {
     const { logout, currentUser } = useAuth()
-    const pathname = usePathname()
 
     if(!currentUser){
         return null
